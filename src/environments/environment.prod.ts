366d0158aa569ed2c3dc89a923466acda3a416d9
RodrigoGGO <rodrigoguerra@id.uff.br>
2022-10-14 20:25:50 -0300

    FEAT - CI/CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  google_client_id:'947755312960-6v602shm9tu5a0sqvortc0u0tql1qcbq.apps.googleusercontent.com'
+  google_client_id:'947755312960-6v602shm9tu5a0sqvortc0u0tql1qcbq.apps.googleusercontent.com',
+  backend_base_url:'https://d1jjp4ddzxq0gc.cloudfront.net/graphql'
 };
