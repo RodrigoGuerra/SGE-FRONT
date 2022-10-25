ec6d101aba80032ae01f62fc008d0739ddb542a1
RodrigoGGO <rodrigoguerra@id.uff.br>
2022-10-25 20:12:41 -0300

    Test deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   google_client_id:'947755312960-6v602shm9tu5a0sqvortc0u0tql1qcbq.apps.googleusercontent.com',
   backend_base_url:'https://d3s6km3vcjamo3.cloudfront.net/graphql'
   // backend_base_url:'http://localhost:3000/graphql'
+  
 };
 
 /*
